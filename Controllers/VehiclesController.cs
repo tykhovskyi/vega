01d9e3e76d52f29d1f4175d9cacf680c2d7f85fe
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-06-26 17:26:16 +0300

    Building an API to Delete Objects

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -47,6 +47,10 @@ namespace vega.Controllers
 
             var vehicle = await context.Vehicles.Include(v => v.Features)
                 .SingleOrDefaultAsync(v => v.Id == id);
+            
+            if (vehicle == null)
+                return NotFound();
+            
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -55,6 +59,20 @@ namespace vega.Controllers
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+            
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
         }
 
         [HttpGet]
