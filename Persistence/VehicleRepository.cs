1c4197464633bc73669d77f9a89f535f2dcf1258
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-06-27 15:40:26 +0300

    Decoupling from Entity Framework

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -13,14 +13,27 @@ namespace vega.Persistence
             this.context = context;
         }
 
-        public async Task<Vehicle> GetVehicle(int id)
+        public async Task<Vehicle> GetVehicle(int id, bool includeRelated = true)
         {
+            if (!includeRelated)
+                return await context.Vehicles.FindAsync(id);
+
             return await context.Vehicles
                 .Include(v => v.Features)
                 .ThenInclude(vf => vf.Feature)
                 .Include(v => v.Model)
                 .ThenInclude(m => m.Make)
                 .FirstOrDefaultAsync(v => v.Id == id);
+        }
+
+        public void Add(Vehicle vehicle)
+        {
+            context.Add(vehicle);
+        }
+
+        public void Remove(Vehicle vehicle)
+        {
+            context.Remove(vehicle);
         }
     }
 }
