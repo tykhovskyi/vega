ece135b80f845f08e3ab9933a63018668ca573b2
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-06-27 12:57:46 +0300

    Refactoring: Using KeyValuePairResource

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,8 +12,9 @@ namespace vega.Mapping
         {
             // Domain to API Resource
             CreateMap<Make, MakeResource>();
-            CreateMap<Model, ModelResource>();
-            CreateMap<Feature, FeatureResource>();
+            CreateMap<Make, KeyValuePairResource>();
+            CreateMap<Model, KeyValuePairResource>();
+            CreateMap<Feature, KeyValuePairResource>();
             CreateMap<Vehicle, SaveVehicleResource>()
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource
                     {
@@ -32,7 +33,7 @@ namespace vega.Mapping
                         Phone = v.ContactPhone
                     }))
                 .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features
-                    .Select(vf => new FeatureResource { Id = vf.Feature.Id, Name = vf.Feature.Name })));
+                    .Select(vf => new KeyValuePairResource { Id = vf.Feature.Id, Name = vf.Feature.Name })));
 
             // API Resource to Domain
             CreateMap<SaveVehicleResource, Vehicle>()
