49504244212732da3a650f5b69cc58bfd379599f
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-06-28 16:40:36 +0300

    Submitting the Form

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -17,5 +17,9 @@ export class VehicleService {
     return this.http.get(this.originUrl + '/api/features')
       .map(res => res.json());
   }
-
+  
+  create(vehicle) {
+    return this.http.post(this.originUrl + '/api/vehicles', vehicle)
+      .map(res => res.json());
+  }
 }
