f558eaa5747a14945751b173ca5afdfc5d8cfda3
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-08-31 11:09:39 +0300

    Sorting on the Server

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -36,7 +36,7 @@ namespace vega.Mapping
                     .Select(vf => new KeyValuePairResource { Id = vf.Feature.Id, Name = vf.Feature.Name })));
 
             // API Resource to Domain
-            CreateMap<FilterResource, Filter>();
+            CreateMap<VehicleQueryResource, VehicleQuery>();
             CreateMap<SaveVehicleResource, Vehicle>()
                 .ForMember(v => v.Id, opt => opt.Ignore())
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
