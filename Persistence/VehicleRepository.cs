0d4b048310ffabccea5373435d9bb4165ceab80e
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-08-31 11:03:26 +0300

    Fixed: Filtering by ModelId is broken

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -49,6 +49,9 @@ namespace vega.Persistence
             if (filter.MakeId.HasValue)
                 query = query.Where(v => v.Model.MakeId == filter.MakeId.Value);
             
+            if (filter.ModelId.HasValue)
+                query = query.Where(v => v.ModelId == filter.ModelId.Value);
+            
             return await query.ToListAsync();
         }
     }
