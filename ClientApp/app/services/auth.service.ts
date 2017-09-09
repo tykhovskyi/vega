726c94fd0c6cc3395c8e7598ccf0e23e2de844b1
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-09-09 12:07:31 +0300

    Setting the Audience for the Token

--- ClientApp/app/services/auth.service.ts
+++ ClientApp/app/services/auth.service.ts
@@ -24,32 +24,30 @@ export class AuthService {
   
   public handleAuthentication(): void {
     this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
+      console.log(authResult)
+      if (authResult && authResult.accessToken) {
         window.location.hash = '';
         this.setSession(authResult);
-        this.router.navigate(['/home']);
       } else if (err) {
-        this.router.navigate(['/home']);
         console.log(err);
       }
+      this.router.navigate(['/vehicles']);
     });
   }
 
   private setSession(authResult): void {
     // Set the time that the access token will expire at
     const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('token', authResult.accessToken);
     localStorage.setItem('expires_at', expiresAt);
   }
 
   public logout(): void {
     // Remove tokens and expiry time from localStorage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
+    localStorage.removeItem('token');
     localStorage.removeItem('expires_at');
     // Go back to the home route
-    this.router.navigate(['/']);
+    this.router.navigate(['/vehicles']);
   }
 
   public isAuthenticated(): boolean {
